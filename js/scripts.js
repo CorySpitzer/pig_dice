13f845dc57f9dc5d2fd4ab82f2fa9340b0a0fbde
phillip shannon & Cory Spitzer <student@epicodus.com>
2015-09-16 09:41:55 -0700

    Add test and function for new game

--- js/scripts.js
+++ js/scripts.js
@@ -3,52 +3,6 @@ function BankAccount(memberName,initialDeposit) {
   this.initialDeposit = initialDeposit;
 }
 
-BankAccount.prototype.depositWithdraw = function(deposit, withdraw) {
-  // if (withdraw == "") {
-  //   withdraw = 0;
-  // } else if (deposit == "") {
-  //   deposit = 0;
-  // }
-  this.initialDeposit = this.initialDeposit + deposit - withdraw;
-
+function Game() {
+  this.turn = 0;
 }
-
-// BankAccount.prototype.withdraw = function(amount) {
-//   this.initialDeposit = this.initialDeposit - amount;
-// }
-
-$(document).ready(function() {
-  var newBankAccount;
-  $("form#new-account").submit(function(event) {
-    // $("span.balance").val("");
-    var inputtedUserName = $("input#new-user-name").val();
-    var inputtedInitialBalance = parseInt($("input#initial-balance").val());
-    newBankAccount = new BankAccount(inputtedUserName, inputtedInitialBalance);
-    $("span.balance").text(newBankAccount.initialDeposit);
-    // $("span.balance").text(newBankAccount.balance);
-    event.preventDefault();
-    // $("input").val("");
-
-  });
-  $("form#deposit-withdraw").submit(function(event) {
-    event.preventDefault();
-    var inputtedDeposit;
-    debugger;
-    if ($("input#deposit").val() == "") {
-      inputtedDeposit = 0;
-    } else {
-      inputtedDeposit = parseInt($("input#deposit").val());
-    }
-    var inputtedWithdrawal;
-
-    if ($("input#withdrawal").val() == "") {
-      inputtedWithdrawal = 0;
-    } else {
-      inputtedWithdrawal = parseInt($("input#withdrawal").val());
-    }
-
-    newBankAccount.depositWithdraw(inputtedDeposit, inputtedWithdrawal);
-    // newBankAccount.withdraw(inputtedWithdrawal);
-    $("span.balance").text(newBankAccount.initialDeposit);
-  });
-});
---
+++ specs/specs.js
@@ -0,0 +1,14 @@
+//Die roll (random number 1-6)
+
+
+//Player class (human or computer; score)
+
+//Game class (type of players, game.TakeTurn, difficulty, variations )
+
+describe('Game', function () {
+  it("makes a new game", function() {
+    var game = new Game();
+    expect(game.turn).to.equal(0);
+    // expect(game.)
+  });
+});
