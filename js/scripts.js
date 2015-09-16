346078ecc1465da41b9b5ada1a62d82bd2c9d3e7
phillip shannon & Cory Spitzer <student@epicodus.com>
2015-09-16 10:43:06 -0700

    Track total roll

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ function Game() {
 function Player() {
   this.score = 0;
 }
+
+// From MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$(document).ready(function() {
+  var total = 0;
+  $("#roll").click(function() {
+    total += randomInt(1,7);
+    $(".total").text(total);
+  });
+});
